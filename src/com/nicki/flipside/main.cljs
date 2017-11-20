c163db56650bfc3b7ce6ca47d46205f77d402bd5
Nicki Vance <nicki@boxingindustries.com>
2017-11-20 17:26:26 +1100

    Add a function for checking that tiles are adjacent to each other (share an edge).

--- src/com/nicki/flipside/main.cljs
+++ src/com/nicki/flipside/main.cljs
@@ -109,7 +109,29 @@
     ;;step forward regularly
     (js/setInterval #(trigger! {:event/tick nil}) 200)))
 
-;;;;;;Tests;;;;;;;
+(defn adjacent-tiles?
+  "given two tiles in the grid, check if they share an edge"
+  [vec1 vec2]
+  (let [[c1 r1] vec1
+        [c2 r2] vec2]
+    ;;first, make sure the two arguments are not the same
+    (if (not= [c1 r1] [c2 r2])
+      ;;now, check to see if the tiles share a row or column
+      (cond
+
+        ;; if the tiles share a column, the rows should be next to each other
+        (and (= c1 c2)
+             (<= -1 (- r1 r2) 1))
+        true
+
+        ;; if the tiles share a row, the columns should be next to each other
+        (and (= r1 r2)
+             (<= -1 (- c1 c2) 1))
+        true
+
+        :else
+        false)
+      false)))
 
 ;;;;;;Tests;;;;;;;
 
