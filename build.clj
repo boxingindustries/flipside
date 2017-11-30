9956dd569236834929fe5c19bbdc28424b661b3a
Nicki Vance <nicki@boxingindustries.com>
2017-11-30 15:15:58 +1100

    Update build preferences.

--- build.clj
+++ build.clj
@@ -1,3 +1,4 @@
 (require 'cljs.build.api)
 
-(cljs.build.api/build "src" {:output-to "resources/public/js/outs/main.js"})
+(cljs.build.api/build "src" {:output-to "resources/public/js/flipside.js"
+                             :output-dir "resources/public/js/out"})
