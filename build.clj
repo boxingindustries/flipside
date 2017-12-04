ea6d3903e9bbdd3c69f946366498aa793241bc71
Nicki Vance <nicki@boxingindustries.com>
2017-12-04 12:34:16 +1100

    Update build file to reflect new file organization for github public site.

--- build.clj
+++ build.clj
@@ -1,4 +1,4 @@
 (require 'cljs.build.api)
 
-(cljs.build.api/build "src" {:output-to "resources/public/js/flipside.js"
-                             :output-dir "resources/public/js/out"})
+(cljs.build.api/build "src" {:output-to "docs/js/flipside.js"
+                             :output-dir "docs/js/out"})
